70e6a3c0c1fa11238619be84faeab780222b2ad7
Rodrigo de Souza Corrêa <correa.rodrigo88@gmail.com>
2023-06-17 17:00:27 -0300

    Atualização 17/06/23

---
+++ src/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <span>Desenvolvido com</span>
+    <span><i class="icofont-heart text-danger"></i></span>
+    <span>por Cod<span class="text-danger">3</span>r</span>
+</footer>
+</body>
+</html>
