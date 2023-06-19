2e692fb1ab1671ed24fbb14ab4eb5f66fde1b813
Rodrigo de Souza Corrêa <correa.rodrigo88@gmail.com>
2023-06-19 19:00:36 -0300

    atualização 19/06

--- src/views/day_records.php
+++ src/views/day_records.php
@@ -1,3 +1,7 @@
 <main class="content">
     <h1>OK!</h1>
+    <h1>OK!</h1>
+    <h1>OK!</h1>
+    <h1>OK!</h1>
+   
 </main>
--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -3,5 +3,6 @@
     <span><i class="icofont-heart text-danger"></i></span>
     <span>por Cod<span class="text-danger">3</span>r</span>
 </footer>
+<script src="assets/js/app.js"></script>
 </body>
 </html>
